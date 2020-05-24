22b83b8bbba32f984bafd767b2d7228654c33d62
Kevin Colten <kevincolten@gmail.com>
2020-05-23 22:12:31 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const postal = require('node-postal');
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.json(
+  postal.parser.parse_address(decodeURIComponent(req.query.address))
+));
+
+app.listen(port, () => console.log(`Example app listening at on port ${port}`))
